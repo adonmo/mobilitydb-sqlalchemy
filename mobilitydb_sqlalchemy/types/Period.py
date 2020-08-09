e480eee6af30ea807d23714d4e2586b254f34dfb
Wendell CTR Turner <Wendell.CTR.Turner@faa.gov>
2020-08-09 14:25:14 -0400

    added Period datatype

---
+++ mobilitydb_sqlalchemy/types/Period.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+from pymeos.temporal import TText
+from sqlalchemy.types import UserDefinedType
+
+from .TBaseType import TBaseType
+
+def period_ident(self,t):
+    return(t)
+
+class Period(TBaseType):
+    pymeos_instant_type = TText
+    pymeos_deserializer_type = period_ident
+
+    def get_col_spec(self):
+        return "PERIOD"
+
+    @staticmethod
+    def validate_type(value):
+        if not str(value):
+            raise TypeError(
+                "Period needs well-formatted string Got: {} Expected {}".format("??", "?")
+            )
+
+    def bind_processor(self, dialect):
+        def process(value):
+            self.validate_type(value)
+            return(value)
+        return process
+
+    def result_processor(self, dialect, coltype):
+        def process(value):
+            return(value)
+        return process
